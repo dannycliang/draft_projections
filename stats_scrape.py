edefe64201b30d6ecbdfc7c1088992604716c05d
Danny <dannyliang.726@berkeley.edu>
2018-08-05 19:38:12 -0400

    added comparisons

--- stats_scrape.py
+++ stats_scrape.py
@@ -80,13 +80,20 @@ def college_stat_scrape(name):
     return stats
 
 def advanced_stats_scrape(players):
-    def add_rpm(rows, players, year):
+    def add_rpm(rows, players, year, rpms):
         indices = {"/year/2014": 2014, "/year/2015": 2015, "/year/2016": 2016, "/year/2017": 2017, "": 2018}
         for row in rows:
             player_name = row.a.text
             if player_name in players:
                 stats = players[player_name]
-                stats["rpm"][indices[year] - stats["draft_year"]] = row.find('td', class_="sortcell").text
+                rpm = row.find('td', class_="sortcell").text
+                stats["rpm"][indices[year] - stats["draft_year"] - 1] = rpm
+                rpm = round(float(rpm), 1)
+                if rpm in rpms:
+                    rpms[rpm].append(str(indices[year]) + " " + player_name)
+                else:
+                    rpms[rpm] = [str(indices[year]) + " " + player_name]
+    rpms = {}
     years = {"/year/2014": 11, "/year/2015": 12, "/year/2016": 11, "/year/2017": 12, "": 14}
     for year in years:
         for page in range(1, years[year] + 1):
@@ -95,8 +102,17 @@ def advanced_stats_scrape(players):
             html = BeautifulSoup(response, 'html.parser')
             oddrows = html.find_all('tr', class_= 'oddrow')
             evenrows = html.find_all('tr', class_= 'evenrow')
-            add_rpm(oddrows, players, year)
-            add_rpm(evenrows, players, year)
+            add_rpm(oddrows, players, year, rpms)
+            add_rpm(evenrows, players, year, rpms)
+    with open('comparisons.csv', mode='w') as stats_file:
+        comp_writer = csv.writer(stats_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        rpm_values = list(rpms.keys())
+        rpm_values.sort()
+        for value in rpm_values:
+            to_write = [value, rpms[value]]
+            comp_writer.writerow(to_write)
+
+
 
 
 def scrape_stats(starting_year, ending_year):
@@ -104,6 +120,7 @@ def scrape_stats(starting_year, ending_year):
     players = {}
     for value in range(starting_year, ending_year + 1):
         print(str(value) + " NBA DRAFT")
+        draft_position = 1
         url = url_template + str(value) + ".html"
         response = get(url).text
         html = BeautifulSoup(response, 'html.parser')
@@ -118,13 +135,18 @@ def scrape_stats(starting_year, ending_year):
             if "player" in characteristic:
                 name = characteristic[starting_index + 3:ending_index - 2].split(",")
                 players[name[1] + " " + name[0]] = college_stat_scrape(name)
-                players[name[1] + " " + name[0]]["rpm"] = ["N/A" for _ in range(2018 - value)]
+                players[name[1] + " " + name[0]]["selection"] = draft_position
+                players[name[1] + " " + name[0]]["rpm"] = ["N/A" for _ in range(15)]
                 players[name[1] + " " + name[0]]["draft_year"] = value
+                draft_position += 1
     advanced_stats_scrape(players)
     with open('stats.csv', mode='w') as stats_file:
         stats_writer = csv.writer(stats_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for player in players:
-            stats_writer.writerow([player] + list(players[player].values()))
+            rpm = players[player].pop("rpm")
+            players[player].pop("draft_year")
+            to_write = [player] + list(players[player].values()) + rpm
+            stats_writer.writerow(to_write)
 
-scrape_stats(2003, 2013)
+scrape_stats(2003, 2017)
 
