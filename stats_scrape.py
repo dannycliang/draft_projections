b5e40a6d34b8270491a85b33748e0d55712da2c1
Danny <dannyliang.726@berkeley.edu>
2018-08-07 01:19:17 -0400

    added all scrape stats

--- stats_scrape.py
+++ stats_scrape.py
@@ -74,9 +74,13 @@ def college_stat_scrape(name):
         tds = html.find_all('td', attrs={'data-stat': True})
         add_basic_stats(stats, tds)
         add_advanced_stats(stats, response)
+        length = 0
         for stat in stats:
             if len(stats[stat]) > 0:
+                if stat == "pts_per_g":
+                    length = len(stats[stat]) - 2
                 stats[stat] = stats[stat][max(len(stats[stat]) - 1, 0)]
+        stats["college_year"] = length
     return stats
 
 def advanced_stats_scrape(players):
@@ -88,11 +92,11 @@ def advanced_stats_scrape(players):
                 stats = players[player_name]
                 rpm = row.find('td', class_="sortcell").text
                 stats["rpm"][indices[year] - stats["draft_year"] - 1] = rpm
-                rpm = round(float(rpm), 1)
+                """rpm = round(float(rpm), 1)
                 if rpm in rpms:
                     rpms[rpm].append(str(indices[year]) + " " + player_name)
                 else:
-                    rpms[rpm] = [str(indices[year]) + " " + player_name]
+                    rpms[rpm] = [str(indices[year]) + " " + player_name]"""
     rpms = {}
     years = {"/year/2014": 11, "/year/2015": 12, "/year/2016": 11, "/year/2017": 12, "": 14}
     for year in years:
@@ -104,15 +108,92 @@ def advanced_stats_scrape(players):
             evenrows = html.find_all('tr', class_= 'evenrow')
             add_rpm(oddrows, players, year, rpms)
             add_rpm(evenrows, players, year, rpms)
-    with open('comparisons.csv', mode='w') as stats_file:
+    """with open('comparisons.csv', mode='w') as stats_file:
         comp_writer = csv.writer(stats_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         rpm_values = list(rpms.keys())
         rpm_values.sort()
         for value in rpm_values:
             to_write = [value, rpms[value]]
-            comp_writer.writerow(to_write)
+            comp_writer.writerow(to_write)"""
 
 
+def all_stats_scrape(players):
+    url_template = "https://www.basketball-reference.com/players/"
+    urls = {}
+    for char in "abcdefghijklmnopqrstuvwyz":
+        url = url_template + char + "/"
+        print(url)
+        response = get(url).text
+        html = BeautifulSoup(response, 'html.parser')
+        rows = html.find_all('th', attrs={'data-append-csv': True})
+        years = html.find_all('td', attrs={'data-stat': True})
+        count = 0
+        for row in rows:
+            if int(years[count].text) > 1996:
+                link = str(row.a)
+                if row.a.text in players:
+                    urls[link[link.index("players"):link.index("html") + 4]] = row.a.text
+            count += 7
+    # page_scrape(urls, players)
+
+
+def page_scrape(urls, players):
+    for url in urls:
+        stats = {"pts_per_g": [],
+                 "trb_per_g": [],
+                 "ast_per_g": [],
+                 "stl_per_g": [],
+                 "blk_per_g": [],
+                 "tov_per_g": [],
+                 "fg3_pct": [],
+                 "ft_pct": [],
+                 "fg_pct": [],
+                 "ts_pct": [],
+                 "bpm": [],
+                 "ws": [],
+                 "ws/48": [],
+                 "vorp": []}
+        player_url = "https://www.basketball-reference.com/" + url
+        response = get(player_url).text
+        html = BeautifulSoup(response, 'html.parser')
+        trs = html.find_all('tr', class_="full_table")
+        for tr in trs:
+            tr = str(tr)
+            stats["fg_pct"].append(tr[tr.index("fg_pct") + 8:tr.index("fg3_per_g") - 35])
+            stats["fg3_pct"].append(tr[tr.index("fg3_pct") + 9:tr.index("fg2_per_g") - 35])
+            stats["ft_pct"].append(tr[tr.index("ft_pct") + 8:tr.index("orb_per_g") - 35])
+            stats["pts_per_g"].append(tr[tr.index("pts_per_g") + 11:tr.index("</tr") - 5])
+            stats["trb_per_g"].append(tr[tr.index("trb_per_g") + 11:tr.index("ast_per_g") - 35])
+            stats["ast_per_g"].append(tr[tr.index("ast_per_g") + 11:tr.index("stl_per_g") - 35])
+            stats["stl_per_g"].append(tr[tr.index("stl_per_g") + 11:tr.index("blk_per_g") - 35])
+            stats["blk_per_g"].append(tr[tr.index("blk_per_g") + 11:tr.index("tov_per_g") - 35])
+            stats["tov_per_g"].append(tr[tr.index("tov_per_g") + 11:tr.index("pf_per_g") - 35])
+        while len(stats["bpm"]) < len(stats["pts_per_g"]):
+            stats["ts_pct"].append(
+                response[response.index(" \" data-stat=\"ts") + 23:response.index(" \" data-stat=\"fg3a_per_fga") - 21])
+            stats["ws"].append(
+                response[response.index(" \" data-stat=\"ws\"") + 19:response.index(" \" data-stat=\"ws_per_48") - 21])
+            stats["ws/48"].append(
+                response[response.index(" \" data-stat=\"ws_per_48") + 26:response.index(" \" data-stat=\"bpm-dum") - 21])
+            stats["bpm"].append(
+                response[response.index(" \" data-stat=\"bpm\"") + 20:response.index(" \" data-stat=\"vorp") - 21])
+            position = response.index(" \" data-stat=\"vorp") + 20
+            response = response[position:]
+            new_position = response.index("</tr>")
+            stats["vorp"].append(
+                response[1:new_position - 5])
+            response = response[new_position:]
+        combined = []
+        for stat in stats:
+            for item in stats[stat]:
+                combined.append(item)
+        print(combined)
+        # players[url]["all"] = combined]
+
+page_scrape(["players/s/smartma01.html"],"")
+
+
+#all_stats_scrape("")
 
 
 def scrape_stats(starting_year, ending_year):
@@ -148,5 +229,6 @@ def scrape_stats(starting_year, ending_year):
             to_write = [player] + list(players[player].values()) + rpm
             stats_writer.writerow(to_write)
 
-scrape_stats(2003, 2017)
 
+
+# scrape_stats(2003, 2018)
